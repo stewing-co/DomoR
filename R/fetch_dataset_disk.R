aab99300c333f9a08e1fe85ad2d96d0d7cffc4c8
Steve Ewing <sewing@central.com>
2025-08-06 08:58:12 -0400

    better csv parsing from fetch

--- R/fetch_dataset_disk.R
+++ R/fetch_dataset_disk.R
@@ -11,36 +11,39 @@
 #' DomoR::init(Sys.getenv('DOMO_BASE_URL'), Sys.getenv('DEVELOPER_TOKEN'))
 #' df <- DomoR::fetch_to_disk(data_source_id="4826e3fb-cd23-468d-9aff-96bf5b690247", nrows=5, delete.tmp.file=TRUE)
 
-fetch_to_disk <- function(data_source_id, nrows=NULL, delete.tmp.file=TRUE) {
-
+fetch_to_disk <- function(data_source_id, nrows = NULL, delete.tmp.file = TRUE) {
+  
   # check that required env variables exist
   if(!exists("customer", .domo_env) || !exists("auth.token", .domo_env)) {
     stop("Both a customer instance and token are required, please set with 'DomoR::init('customer', 'token')'")
   }
   
   get_url <- paste0(.domo_env$customer.url, '/api/data/v2/datasources/', data_source_id, '/dataversions/latest?includeHeader=true')
-
-  all.headers <- httr::add_headers(c(.domo_env$auth.token, .domo_env$user.agent, 'Accept'='text/csv'))
-
-  tmp_file <- tempfile(fileext=".csv")
-
-  print(paste0("Temp File Location :: ", tmp_file))
-
-  get_result <- httr::GET(get_url, all.headers, .domo_env$config, httr::write_disk(tmp_file, overwrite=FALSE))
-
-  # handle errors
+  all.headers <- httr::add_headers(c(.domo_env$auth.token, .domo_env$user.agent, 'Accept' = 'text/csv'))
+  
+  tmp_file <- tempfile(fileext = ".csv")
+  message("Temp File Location :: ", tmp_file)
+  
+  get_result <- httr::GET(get_url, all.headers, .domo_env$config, httr::write_disk(tmp_file, overwrite = FALSE))
   httr::stop_for_status(get_result)
-
-  if(is.null(nrows)){
-    df <- read.csv(file=tmp_file, header = TRUE, sep = ",")
-  }else{
-    df <- read.csv(file=tmp_file, header = TRUE, sep = ",", nrows=nrows)
+  
+  # Guess encoding
+  guessed <- readr::guess_encoding(tmp_file)
+  encoding <- ifelse(is.null(guessed), "UTF-8", guessed$encoding[1])
+  if (encoding == "ASCII") encoding <- "UTF-8"
+  
+  # Read CSV robustly
+  if (is.null(nrows)) {
+    df <- readr::read_csv(tmp_file, locale = readr::locale(encoding = encoding), show_col_types = FALSE)
+  } else {
+    df <- readr::read_csv(tmp_file, locale = readr::locale(encoding = encoding), n_max = nrows, show_col_types = FALSE)
   }
-
-  if(delete.tmp.file){
+  
+  if (delete.tmp.file) {
     file.remove(tmp_file)
-    print(paste0("Temp file deleted successfully from location :: ", tmp_file))
+    message("Temp file deleted successfully from location :: ", tmp_file)
   }
-
-  return(df)
+  
+  # For compatibility with previous version, return as data.frame not tibble
+  return(as.data.frame(df))
 }
